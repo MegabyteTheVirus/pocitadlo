9318363147d219ddfb6c498fbc4e434f078b9fe4
MegabyteTheVirus <169033785+MegabyteTheVirus@users.noreply.github.com>
2024-05-06 10:58:15 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ basic.forever(function () {
     for (let index = 0; index < 1; index++) {
         for (let pořadí = 0; pořadí <= 10; pořadí++) {
             music._playDefaultBackground(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.InBackground)
+            music.setVolume(35)
             basic.showNumber(pořadí)
         }
     }
